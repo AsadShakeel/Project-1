62c5ed36b6939fe9d4956ada2618384fcbf02083
Asad Shakeel <m.asadshakeel@gmail.com>
2017-07-05 20:37:54 +0500

    changes in edit profile fragment

--- app/src/main/java/com/lab42/maham/senseilocater/EditProfileFragment.java
+++ app/src/main/java/com/lab42/maham/senseilocater/EditProfileFragment.java
@@ -1,7 +1,7 @@
 package com.lab42.maham.senseilocater;
 
-
 import android.Manifest;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -9,21 +9,36 @@ import android.test.mock.MockPackageManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EditProfileFragment extends Fragment {
+public class EditProfileFragment extends Fragment implements View.OnClickListener{
 
     private static final int REQUEST_CODE_PERMISSION = 2;
     String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
     GPSTracker gps;
 
     ToggleButton toogle;
+    Button btn_edit = null;
+    EditText et_name = null;
+    EditText et_education = null;
+    EditText et_experience = null;
+    Button btn_submit = null;
+    ImageView img_edit_profile_activity = null;
 
     public EditProfileFragment() {
         // Required empty public constructor
@@ -87,7 +102,171 @@ public class EditProfileFragment extends Fragment {
                 }
             }
         });
+
+        et_name = (EditText) view.findViewById(R.id.et_name);
+        et_education = (EditText) view.findViewById(R.id.et_education);
+        et_experience = (EditText) view.findViewById(R.id.et_experience);
+        btn_edit = (Button) view.findViewById(R.id.btn_edit);
+        btn_submit = (Button) view.findViewById(R.id.btn_submit);
+        img_edit_profile_activity = (ImageView) view.findViewById(R.id.img_edit_profile_activity);
+
+        btn_edit.setOnClickListener(this);
+        btn_submit.setOnClickListener(this);
+
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_edit:
+
+                //code to get image from gallery and upload
+                Toast.makeText(getContext(),"Edit image here", Toast.LENGTH_SHORT).show();
+
+                break;
+            case R.id.btn_submit:
+                Toast.makeText(getContext(),"asadasadasad",Toast.LENGTH_SHORT).show();
+                Teacher t = new Teacher();
+                et_name.toString();
+                t.setId("7");
+                t.setName(et_name.getText().toString());
+                t.setEducation(et_education.getText().toString());
+
+                t.setEmail("lkjlkj");
+                t.setLocation("pk");
+                t.setPassword("lkjlkj");
+                t.setPost("Junior");
+                t.setAvailable("false");
+
+                new MyAsyncTask().execute(t.toString(), t.getId());
+                break;
+            default:
+                break;
+        }
+    }
+
+    public class Teacher{
+        private String Id;
+        private String Name;
+        private String Email;
+        private String Location;
+        private String Education;
+        private String Password;
+        private String Post;
+        private String Available;
+
+        public String getId() {
+            return Id;
+        }
+
+        public String getName() {
+            return Name;
+        }
+
+        public void setName(String name) {
+            Name = name;
+        }
+
+        public String getEmail() {
+            return Email;
+        }
+
+        public void setEmail(String email) {
+            Email = email;
+        }
+
+        public String getLocation() {
+            return Location;
+        }
+
+        public void setLocation(String location) {
+            Location = location;
+        }
+
+        public String getEducation() {
+            return Education;
+        }
+
+        public void setEducation(String education) {
+            Education = education;
+        }
+
+        public String getPassword() {
+            return Password;
+        }
+
+        public void setPassword(String password) {
+            Password = password;
+        }
+
+        public String getPost() {
+            return Post;
+        }
+
+        public void setPost(String post) {
+            Post = post;
+        }
+
+        public String getAvailable() {
+            return Available;
+        }
+
+        public void setAvailable(String available) {
+            Available = available;
+        }
+
+        public void setId(String id) {
+            this.Id = id;
+        }
+    }
+
+
+    public class MyAsyncTask extends AsyncTask<String , String , String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String t = params[0];
+            String id = params[1];
+
+            HttpURLConnection urlConnection = null;
+            URL url = null;
+            try {
+                url = new URL("http://senseilocatorwebservices.apphb.com/api/Teacher/");// + id);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                JSONObject obj = new JSONObject();
+                obj.put("Id", "7");
+                obj.put("Name", "Fahad");
+                obj.put("Email","fff");
+                obj.put("Location","fati");
+                obj.put("Education","Phd");
+                obj.put("Password","123");
+                obj.put("Post","junior");
+                obj.put("Available","false");
+
+                String temp = obj.toString();
+
+                DataOutputStream wr = new DataOutputStream(urlConnection.getOutputStream());
+                wr.writeBytes(temp);
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            finally {
+                urlConnection.disconnect();
+            }
+            return "Successful";
+        }
+
+        @Override
+        protected void onPostExecute(String o) {
+            super.onPostExecute(o);
+            Toast.makeText(getContext(),o,Toast.LENGTH_SHORT).show();
+        }
+    }
+    
 }
